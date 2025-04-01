9716bd3c2f8f18ff8cd7f59e590741d0d685d4f2
Samuel Tafnes <samuel.tafnes.dias@gmail.com>
2025-04-01 19:24:50 -0300

    Atualizado index, css e javascript da pagina

--- main.js
+++ main.js
@@ -1,47 +1,84 @@
+
+/* ======= Alterar Guias ======= */
 const trocar_guia = document.querySelector('.contatos_tarefas');
 const guia_contatos = document.querySelector('.guia_contatos');
 const guia_tarefas = document.querySelector('.guia_tarefas');
 
+/* ======= Icone das opções ======= */
+const imgTelefone = `<img src="./images/call.svg" alt="Ligar">`
+const imgWhatsapp = `<img src="./images/whatsapp.svg" alt="Whatsapp">`
+const imgEmail = `<img src="./images/mail.svg" alt="Enviar e-mail"></img>`
+const imgExcluir = `<img class="removerContato" src="./images/excluir.svg" alt="Apagar contato" title="Remover contato">`;
+
+/* ======= Formulário e Tabela ======= */
 const form_contatos = document.getElementById('formulario_contatos');
+const tabela = document.querySelector("#tabela_contatos");
 
-
+var linhas = '';
 
 
 form_contatos.addEventListener('submit', function(e){
     e.preventDefault();
-    
 
-    const nome_contato = document.getElementById('nome');
-    console.log(nome_contato.value);
+    prepararLinha();
+    adicionarLinha();
+})
+tabela.addEventListener('click', function(evento){
+    var btnRemoverClicado = evento.target;
 
-    const tel_contato = document.getElementById('telefone');
-    console.log(tel_contato.value);
-
-    const mail_contato = document.getElementById('e_mail');
-    console.log(mail_contato.value);
-
-    console.log(tel_contato.value);
+    if (btnRemoverClicado.classList.contains("removerContato")){
+        var celula = btnRemoverClicado.parentNode;
+        var conjuntoOpcoes = celula.parentNode;
+        var linhaSelecionada = conjuntoOpcoes.parentNode;
+        linhaSelecionada.remove();
+    }
 })
 
+function prepararLinha(){
+    const nomeContato = document.getElementById('nome');
+    const telefoneContato = document.getElementById('telefone');
+    const mailContato = document.getElementById('e_mail');
 
+    let linha = `<tr>`;
+    linha += `<td> ${nomeContato.value} </td>`;
+    linha += `<td> ${telefoneContato.value} </td>`
+    linha += `<td> ${mailContato.value} </td>`
+    linha += prepararOpcoes(telefoneContato.value, mailContato.value);
+    linha += `<tr>` 
 
+    linhas += linha;
 
-function formatar(mascara, documento){
-	var i = documento.value.length;
-	var saida = mascara.substring(0,1);
-	var texto = mascara.substring(i)
+    nomeContato.value = '';
+    telefoneContato.value = '';
+    mailContato.value = '';
+}
 
-	if (texto.substring(0,1) != saida){
-		documento += texto.substring(0,1);
-	}
+function prepararOpcoes(telefone, email){
+    let opcao = `<td class="tabela_alinhada">`;
+    opcao += `<a href="tel:${telefone}" title="Ligar para este contato"> ${imgTelefone} </a>`;
+    if (telefone.length > 10){
+        opcao += `<a href="http://wa.me/${telefone}" title="Entrar em contato pelo WhatsApp" target="blank"> ${imgWhatsapp} </a>`;
+    }
+    if (email != ""){
+        opcao += `<a href="mailto:${email}" title="Enviar um E-mail"> ${imgEmail} </a>`;
+    }
+    opcao += `<span> ${imgExcluir} </span>`;
+    opcao += `</td>`;
+
+    return opcao;
+}
+
+function adicionarLinha(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
 }
 
 
 
-
-
-
-
+function validarNome(nomeContato){
+    const nomeDividido = nomeContato.split(' ');
+    return nomeDividido >= 2;
+}
 
 
 function switch_contacts(){
